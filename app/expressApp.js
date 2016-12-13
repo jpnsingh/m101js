4da4e520e9d080c749a6df2b31cf22ded57ace23
jpnsingh <jpnsingh86@gmail.com>
2016-12-13 15:23:57 +0530

    Connecting to mongo db and handling the default express route within the callback;

--- app/expressApp.js
+++ app/expressApp.js
@@ -3,21 +3,33 @@
 
     var express = require('express'),
         engines = require('consolidate'),
+        MongoClient = require('mongodb').MongoClient,
+        assert = require('assert'),
         app = express();
+
+    var url = 'mongodb://localhost:27017/movies';
 
     app.engine('html', engines.nunjucks);
     app.set('view engine', 'html');
     app.set('views', __dirname + '/views');
 
-    app.get('/', function (request, response) {
-        response.render('hello', {name: 'Templates!!'});
-    });
 
-    app.use(function (request, response) {
-        response.sendStatus(404);
-    });
+    MongoClient.connect(url, function (error, db) {
+        assert.equal(null, error);
+        console.log('Successfully connected to MongoDB.');
 
-    var server = app.listen(8001, function () {
-        console.log('Server listening on port %s', server.address().port);
+        app.get('/', function (request, response) {
+            db.collection('movies').find({}).toArray(function (error, docs) {
+                response.render('movies', {movies: docs});
+            });
+        });
+
+        app.use(function (request, response) {
+            response.sendStatus(404);
+        });
+
+        var server = app.listen(8001, function () {
+            console.log('Server listening on port %s', server.address().port);
+        });
     });
 })();
